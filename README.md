fc20684a65d5e2063edad5f2e13bfd6bee1434d1
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2022-07-29 15:06:07 +0530

    Parshu
    Parshu uses regex to filter out the custom results. Remembering every regex or writing regexes for a task which you do daily is not easy, so parshu will help you to automate the task.

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# Parshu
-Filter URLs to save your time.
+<h1 align="center">
+  <br>
+  <a href="https://github.com/R0X4R/Parshu"><img src=".github/static/logo.png" alt="Parshu logo"></a>
+</h1>
+
+<h4 align="center"><b>Filter URLs to save your time using regex</b></h4>
+
+<p align="center">
+  <a href="https://github.com/R0X4R/Parshu/releases">
+    <img src="https://img.shields.io/github/release/R0X4R/Parshu.svg?label=version">
+  </a>
+  <a href="https://github.com/R0X4R/Parshu/releases">
+    <img src="https://img.shields.io/github/downloads/R0X4R/Parshu/total">
+  </a>
+  <a href="https://github.com/R0X4R/Parshu/issues?q=is%3Aissue+is%3Aclosed">
+      <img src="https://img.shields.io/github/issues-closed-raw/R0X4R/Parshu?color=dark-green&label=issues%20fixed">
+  </a>
+  <a href="https://travis-ci.com/R0X4R/Parshu">
+      <img src="https://img.shields.io/travis/com/R0X4R/Parshu.svg?color=dark-green&label=tests">
+  </a>
+</p>
+
+<p align="center"><img src=".github/static/usage.png" alt="Parshu usage"></p>
+
+---
+
+**Parshu** uses regex to filter out the custom results. Remembering every regex or writing regexes for a task which you do daily is not easy, so **parshu** will help you to automate the task.
+
+_Inspired from [tomnomnom's gf](https://github.com/tomnomnom/gf) tool_
+
+### Features
+- You can also add your custom regex to get results
+- Filter out all the query parameters which you use to check vulnerabilities
+
+### Installation
+
+
+```console
+$ pip3 install parshu
+```
+
+```console
+$ wget -O parshu https://raw.githubusercontent.com/R0X4R/Parshu/main/parshu.py -q && chmod +x parshu && mv parshu /usr/bin/
+```
+
+### Usage
+
+<p align="center"><img src=".github/static/usage-2.png" alt="Parshu usage"></p>
+
++ **For `linux`, `unix` and `debian` based systems**
+
+    ```console
+    $ waybackurls sub.domain.tld | parshu
+
+    http://sub.domain.tld/wvstests/
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19/scripts
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19/scripts/
+    ```
+
++ **For `windows` based systems**
+
+    ```console
+    cmd> type urls.txt | python3 parshu.py
+    ```
+
++ **If `no arguments` passed**
+
+    ```console
+    $ waybackurls sub.domain.tld | parshu
+
+    http://sub.domain.tld/wvstests/
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19/scripts
+    http://sub.domain.tld/wvstests/pmwiki_2_1_19/scripts/
+    ```
+
++ **To filter out the results for xss**
+
+    > **Note**: To get parameters for other vulnerabilites use other arguments. To get all the arguments use `parshu -h`
+
+    ```console
+    $ waybackurls testphp.vulnweb.com | parshu -x
+
+    http://testphp.vulnweb.com/artists.php?artist=1
+    http://testphp.vulnweb.com/showimage.php?file=./pictures/1.jpg
+    http://testphp.vulnweb.com/showimage.php?file=./pictures/1.jpg&size=160
+    ```
+
+### Donate
+If this tool helped you or you like my work
+
+</br><a href="https://www.buymeacoffee.com/R0X4R"><img src="https://img.buymeacoffee.com/button-api/?text=Help me to buy oscp&emoji=😇&slug=R0X4R&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"/></a> <a style=" width: 135px; background-color: #1065b7; text-align: center; font-weight: 800; padding: 11px 0px; color: white; font-size: 12px; display: inline-block; text-decoration: none; " href='https://pmny.in/bIKNZngt4ys1'> Donate Now </a> <a href="https://ko-fi.com/i/IK3K34SJSA"><img src="https://ko-fi.com/img/githubbutton_sm.svg"></a><br/><br/>
---
+++ parshu.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+"""
+Author: Eshan Singh (R0X4R)
+Version: 1.0.0
+"""
+
+#@> IMPORTING ALL THE DEPENDENCIES
+import re
+import argparse
+from urllib.parse import urlparse
+from sys import stdin, stdout, exit
+
+#@> PROCESSING COMMAND LINE ARGUMENTS
+parser = argparse.ArgumentParser(epilog="\tExample:\nwaybackurls sub.domain.tld | tool")
+parser._optionals.title = "OPTIONS"
+
+#@> OPTIONS
+parser.add_argument('-x', dest='xss', action='store_true', help="Filter all URLS where you check for xss.")
+parser.add_argument('-r', dest='redirect', action='store_true', help="Filter all URLS where you check for open-redirect.")
+parser.add_argument('-l', dest='lfi', action='store_true', help="Filter all URLS where you check for lfi.")
+parser.add_argument('-s', dest='sql', action='store_true', help="Filter all URLS where you check for sqli.")
+parser.add_argument('-t', dest='ssti', action='store_true', help="Filter all URLS where you check for ssti.")
+parser.add_argument('-f', dest='ssrf', action='store_true', help="Filter all URLS where you check for ssrf.")
+parser.add_argument('-c', dest='rce', action='store_true', help="Filter all URLS where you check for rce.")
+parser.add_argument('-q', dest='query', action='store_true', help="Filter all URLS which contains query parameters.")
+
+cmd = parser.parse_args()
+
+#@> REGEX PATTERNS
+PATTERN = "(\.asp|\.aspx|\.bat|\.cfm|\.cgi|\.css|\.dll|\.exe|\.htm|\.html|\.inc|\.jhtml|\.js|\.jsa|\.jsp|\.log|\.mdb|\.nsf|\.pcap|\.php|\.php2|\.php3|\.php4|\.php5|\.php6|\.php7|\.phps|\.pht|\.phtml|\.pl|\.reg|\.sh|\.shtml|\.sql|\.swf|\.txt|\.xml|\.ini|\,xml|\.bat|\.LOG|\.tn|\.bak|\.sql)"
+XSS_REGEX = "(api=|api_key=|begindate=|callback=|=|categoryid=|csrf_token=|email=|emailto=|enddate=|id=|imagine=|immagine=|item=|jsonp=|key=|keyword=|keywords=|l=|lang=|list_type=|month=|name=|p=|page=|page_id=|password=|pid=|pp=|q=|query=|s=|search=|terms=|token=|type=|unsubscribe_token=|url=|username=|view=|year=)"
+RED_REGEX = "(Lmage_url=|Open=|callback=|cgi-bin/redirect.cgi|cgi-bin/redirect.cgi?|checkout=|checkout_url=|continue=|data=|dest=|destination=|dir=|domain=|feed=|file=|file_name=|file_url=|folder=|folder_url=|forward=|from_url=|go=|goto=|host=|html=|image_url=|img_url=|load_file=|load_url=|login?to=|login_url=|logout=|navigation=|next=|next_page=|out=|page=|page_url=|path=|port=|redir=|redirect=|redirect_to=|redirect_uri=|redirect_url=|reference=|return=|returnTo=|return_path=|return_to=|return_url=|rt=|rurl=|show=|site=|target=|to=|uri=|url=|val=|validate=|view=|window=)"
+SQL_REGEX = "(id=|select=|report=|role=|update=|query=|user=|name=|sort=|where=|search=|params=|process=|row=|view=|table=|from=|sel=|results=|sleep=|fetch=|order=|keyword=|column=|field=|delete=|string=|number=|filter=)"
+STI_REGEX = "(template=|preview=|id=|view=|activity=|name=|content=|redirect=)"
+SRF_REGEX = "(access=|admin=|dbg=|debug=|edit=|grant=|test=|alter=|clone=|create=|delete=|disable=|enable=|exec=|execute=|load=|make=|modify=|rename=|reset=|shell=|toggle=|adm=|root=|cfg=|dest=|redirect=|uri=|path=|continue=|url=|window=|next=|data=|reference=|site=|html=|val=|validate=|domain=|callback=|return=|page=|feed=|host=|port=|to=|out=|view=|dir=|show=|navigation=|open=|file=|document=|folder=|pg=|php_path=|style=|doc=|img=|filename=)"
+LFI_REGEX = "(file=|document=|folder=|root=|path=|pg=|style=|pdf=|template=|php_path=|doc=|page=|name=|cat=|dir=|action=|board=|date=|detail=|download=|prefix=|include=|inc=|locate=|show=|site=|type=|view=|content=|layout=|mod=|conf=|url=)"
+RCE_REGEX = "(daemon=|upload=|dir=|download=|log=|ip=|cli=|cmd=|exec=|command=|execute=|ping=|query=|jump=|code=|reg=|do=|func=|arg=|option=|load=|process=|step=|read=|function|req=|feature=|exe=|module=|payload=|run=|print=)"
+
+try:
+    if stdin.isatty():
+        exit(0)
+        
+    if cmd.xss:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains xss parameters.
+            """
+            link = str(URL.strip())
+            if re.search(XSS_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+
+    elif cmd.redirect:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains open-redirect parameters.
+            """
+            link = str(URL.strip())
+            if re.search(RED_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    elif cmd.lfi:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains lfi parameters.
+            """
+            link = str(URL.strip())
+            if re.search(LFI_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    elif cmd.sql:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains sql parameters.
+            """
+            link = str(URL.strip())
+            if re.search(SQL_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    elif cmd.ssti:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains ssti parameters.
+            """
+            link = str(URL.strip())
+            if re.search(STI_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    elif cmd.ssrf:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains ssrf parameters.
+            """
+            link = str(URL.strip())
+            if re.search(SRF_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    elif cmd.rce:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains rce parameters.
+            """
+            link = str(URL.strip())
+            if re.search(RCE_REGEX, link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+
+    elif cmd.query:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS which contains query parameters.
+            """
+            link = str(URL.strip())
+            if re.search("(=|&)", link):
+                stdout.write(re.sub(r"'|(|)", "", link) + '\n')
+    
+    else:
+        for URL in stdin.readlines():
+            """
+            STDOUT all the URLS except which contains query parameters and files.
+            """
+            links = re.split('/', urlparse(URL.strip()).path)[-1]
+
+            if re.search(PATTERN, links):
+                output = re.sub(r"([^/\\]+)(\.php|\.p|\%|\.asp|\.aspx|\.bat|\.cfm|\.cgi|\.css|\.dll|\.exe|\.htm|\.html|\.inc|\.jhtml|\.js|\.jsa|\.jsp|\.log|\.mdb|\.nsf|\.pcap|\.php2|\.php3|\.php4|\.php5|\.php6|\.php7|\.phps|\.pht|\.phtml|\.pl|\.reg|\.sh|\.shtml|\.sql|\.swf|\.txt|\.xml|\.ini|\,xml|\.bat|\.LOG|\.tn|\.bak|\.sql).*", "", str(URL.strip()))
+                stdout.write(re.sub(r"'|(|)", "", output) + '\n')
+
+            elif re.search(r"(&|=|\?|\%)", URL.strip()):
+                pass
+
+            else:
+                link= urlparse(URL.strip()).scheme + "://" + urlparse(URL.strip()).netloc + urlparse(URL.strip()).path
+                output = re.sub(r"([^/\\]+)(\.php|\.p|\%).*", "", link)
+                stdout.write(str(re.sub(r"'|(|)", "", output)) + '\n')
+except KeyboardInterrupt:
+    exit(0)
+except:
+    pass
